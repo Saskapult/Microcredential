1e0a6e787b7e4aa9a5b0d7ae6ba56f17f4b40fc2
Saskapult <thekieransmith@gmail.com>
2024-08-16 09:49:12 -0700

    slug jumping

--- Assets/Scripts/SlugMovement.cs
+++ Assets/Scripts/SlugMovement.cs
@@ -8,6 +8,9 @@ public class SlugMovement : MonoBehaviour
 {
     public float speed = 3.0f;
     public float rotationSpeed = 75.0f;
+    public float jumpForce = 5.0f;
+
+    private bool jumpTime = false;
 
     private Rigidbody body;
 
@@ -15,6 +18,16 @@ public class SlugMovement : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (onGround() && Input.GetButtonDown("Jump")) {
+            jumpTime = true;
+        }
+    }
+
+    bool onGround() {
+        return Physics.OverlapSphere(transform.position, 0.05f, LayerMask.GetMask("Ground")).Length != 0;
     }
 
     // Update is called once per frame
@@ -25,7 +38,9 @@ public class SlugMovement : MonoBehaviour
         Vector3 dir = (transform.right * horizontal + transform.forward * vertical).normalized;
         body.MovePosition(transform.position + dir * speed * Time.fixedDeltaTime);
 
-        // var horizontal = Input.GetAxis("Horizontal");
-        // transform.Rotate(Vector3.up, horizontal * rotationSpeed * Time.fixedDeltaTime, 0f);
+        if (jumpTime) {
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpTime = false;
+        }
     }
 }
